91577e6d06fd470719d23c79e833f2c05884e985
aytiqaqash <aytiqaqash@gmail.com>
2023-08-06 23:13:16 -0400

    Add Jackson to convert JSON String to Object
    Description:
    Jackson helps us to convert JSON to Object and ignore unused fields.

--- src/main/java/dev/aytiqaqash/courseinfo/cli/service/CourseRetrieverService.java
+++ src/main/java/dev/aytiqaqash/courseinfo/cli/service/CourseRetrieverService.java
@@ -1,5 +1,9 @@
 package dev.aytiqaqash.courseinfo.cli.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,6 +20,10 @@ public class CourseRetrieverService {
             .followRedirects(HttpClient.Redirect.ALWAYS)
             .build();
 
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+
     public List<PluralsightCourse> getCoursesFor(String authorId){
         HttpRequest request = (HttpRequest) HttpRequest
                 .newBuilder(URI.create(PS_URI.formatted(authorId)))
@@ -25,12 +33,18 @@ public class CourseRetrieverService {
         try {
             HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
             return switch (response.statusCode()){
-                case 200 -> null;
+                case 200 -> toPluralsightCourses(response);
                 case 404 -> List.of();
                 default -> throw new RuntimeException("Pluralsight API call failed with status code "+ response.statusCode());
             };
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Could not call Pluralsight API ", e);
         }
+    }
+
+    private static List<PluralsightCourse> toPluralsightCourses(HttpResponse<String> response) throws JsonProcessingException {
+        JavaType returnType = OBJECT_MAPPER.getTypeFactory()
+                        .constructCollectionType(List.class, PluralsightCourse.class);
+        return OBJECT_MAPPER.readValue(response.body(), returnType);
     }
 }
