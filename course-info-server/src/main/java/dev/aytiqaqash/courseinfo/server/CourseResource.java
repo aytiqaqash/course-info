5ac979dcc230a308b5bc534251326f6872f7ab03
aytiqaqash <aytiqaqash@gmail.com>
2023-08-09 20:54:36 -0400

    Convert Get response to the JSON from string
    Description:
    jersey-media-json-jackson helps us to return JSON instead String in Get courses endpoint.

--- course-info-server/src/main/java/dev/aytiqaqash/courseinfo/server/CourseResource.java
+++ course-info-server/src/main/java/dev/aytiqaqash/courseinfo/server/CourseResource.java
@@ -2,11 +2,17 @@ package dev.aytiqaqash.courseinfo.server;
 
 import dev.aytiqaqash.courseinfo.domain.Course;
 import dev.aytiqaqash.courseinfo.repository.CourseRepository;
+import dev.aytiqaqash.courseinfo.repository.RepositoryException;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Path("/courses")
@@ -20,11 +26,18 @@ public class CourseResource {
     }
 
     @GET
-    public String getCourses(){
-        return courseRepository
-                .getALlCourses()
-                .stream()
-                .map(Course::toString)
-                .collect(Collectors.joining(", "));
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Course> getCourses(){
+        try {
+            return courseRepository
+                    .getALlCourses()
+                    .stream()
+                    .sorted(Comparator.comparing(Course::id))
+                    .toList();
+        }catch (RepositoryException e){
+            LOG.error("Could not retrieve courses from the database", e);
+            throw new NotFoundException();
+        }
+
     }
 }
